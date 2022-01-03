66c8a6d0784f73a669000ee3830261939b11974a
kindo_git <37190803+KindoIssouf@users.noreply.github.com>
2022-01-03 00:08:27 -0500

    Memoization
    use meno to evoid repeting work

--- allconstructs.js
+++ allconstructs.js
@@ -1,12 +1,16 @@
 
-const allConstruct = (target, wordBank) =>{
+const allConstruct = (target, wordBank, meno = {}) =>{
+  if (target in meno) return meno[target];
   if(target === '') return [[]];
   var result = []
   for (let word of wordBank){
     if(target.indexOf(word) === 0){  
       newss = target.slice(word.length)
-     let  waysFromChild =  allConstruct(newss, wordBank);
-     let targetWays = waysFromChild.map(way => [word, ...way])
+     let  waysFromChild =  allConstruct(newss, wordBank, meno);
+     let targetWays = waysFromChild.map(way => [word, ...way]) 
+      /** this will take every sub array and add word tp the begining**/ 
+      meno[target] = targetWays;
+      
      result.push(...targetWays)
     }
    
@@ -23,3 +27,10 @@ console.log(allConstruct("purple", ["purp", 'p', 'ur','le', 'purpl']))
   ['p', 'ur', 'p','le']
   ]
 */
+
+console.log(allConstruct("eeeeeeeeeeeeeeeeef", ["ee", 'e', 'ee','e', 'eee']))
+/**
+  
+  []
+*/
+
